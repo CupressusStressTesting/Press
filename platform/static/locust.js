586bb2984f79954589f2e08cdbc9b7012155e903
ReenExe <ipourri@gmail.com>
2015-10-20 22:35:23 +0300

    move mock to server

--- platform/static/locust.js
+++ platform/static/locust.js
@@ -1,181 +1,11 @@
 (function () {
+    var socket = io();
+
+    socket.on('connected', console.log);
+
     var Service = {
         getRequestStatistic: function (success) {
-            var mock = {
-                "errors": [],
-                "stats": [{
-                    "median_response_time": 4,
-                    "min_response_time": 4,
-                    "current_rps": 0.0,
-                    "name": "/",
-                    "num_failures": 0,
-                    "max_response_time": 4,
-                    "avg_content_length": 23,
-                    "avg_response_time": 4.0,
-                    "method": "GET",
-                    "num_requests": 1
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/1/element/1",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/1/element/2",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/1/element/3",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/1/elements",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/2/element/1",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/2/element/2",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/2/element/3",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/2/elements",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/3/element/1",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/3/element/2",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/3/element/3",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 0,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "/api/collection/3/elements",
-                    "num_failures": 0,
-                    "max_response_time": 0,
-                    "avg_content_length": 0,
-                    "avg_response_time": 0,
-                    "method": "GET",
-                    "num_requests": 0
-                }, {
-                    "median_response_time": 5,
-                    "min_response_time": 5,
-                    "current_rps": 0.0,
-                    "name": "/api/collections",
-                    "num_failures": 0,
-                    "max_response_time": 5,
-                    "avg_content_length": 177,
-                    "avg_response_time": 5.0,
-                    "method": "GET",
-                    "num_requests": 1
-                }, {
-                    "median_response_time": 4,
-                    "min_response_time": 0,
-                    "current_rps": 0.0,
-                    "name": "Total",
-                    "num_failures": 0,
-                    "max_response_time": 5,
-                    "avg_content_length": 100,
-                    "avg_response_time": 4.5,
-                    "method": null,
-                    "num_requests": 2
-                }],
-                "state": "running",
-                "total_rps": Math.floor(Math.random() * 10),
-                "fail_ratio": Math.floor(Math.random() * 3),
-                "user_count": 12
-            };
-
-            success(mock);
+            socket.on('request.statistic', success);
         },
         getExceptions: function (success) {
             var mock = {"exceptions": []};
@@ -188,10 +18,6 @@
         if ($("#locust_count").length > 0) {
             $("#locust_count").focus().select();
         }
-
-        var socket = io();
-
-        socket.on('connected', console.log);
     });
 
     $('#js-stop a').click(function (event) {
@@ -308,8 +134,6 @@
             $("#js-user-count").html(report.user_count);
 
             show(report);
-
-            setTimeout(updateStats, 2000);
         });
     }
 
